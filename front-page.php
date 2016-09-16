b1c859182c4f4203b7df3664fea6d3b25c614638
Ivan26ru <it-1ivan@ya.ru>
2016-09-16 17:15:42 +0300

    Добавлен шаблон начальной страницы

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Шаблон НАЧАЛЬНОЙ страницы (front-page.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+get_header(); // подключаем header.php ?>
+
+<?php get_footer(); // подключаем footer.php ?>
