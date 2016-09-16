9e9aa22e7f593fc72f8375a178b2476385c43fdb
Ivan26ru <it-1ivan@yandex.ru>
2016-09-16 23:33:19 +0300

    Добавлен заголовок и название ячеек по ремонту техники

--- front-page.php
+++ front-page.php
@@ -9,12 +9,13 @@ get_header(); // подключаем header.php ?>
 <div class="clearfix"></div>
 <br>
 <br>
+<h1 class="text-center">Ремонт техники</h1>
 <div class="container">
 <div class="row">
 	<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
 		<div class="text-center">
         	<img src="http://placehold.it/220x240" alt="..." class="">
-        	<h4>Ремонт ТОВАРА</h4>
+        	<h4>Ремонт телефонов</h4>
         	<ul class="dn">
         		<li>lonem01</li>
         		<li>lonem02</li>
@@ -27,30 +28,30 @@ get_header(); // подключаем header.php ?>
         		<li>lonem09</li>
         		<li>lonem10</li>
         	</ul>
-        </div>	
+        </div>
 	</div>
-	
-	<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-		<div class="text-center">	
-        	<img src="http://placehold.it/220x240" alt="..." class="">
-        	<h4>Ремонт ТОВАРА</h4>
-        </div>	
-	</div>
-	
+
 	<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
 		<div class="text-center">
         	<img src="http://placehold.it/220x240" alt="..." class="">
-        	<h4>Ремонт ТОВАРА</h4>
-        </div>	
+        	<h4>Ремонт планшетов</h4>
+        </div>
 	</div>
-	
+
 	<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
 		<div class="text-center">
         	<img src="http://placehold.it/220x240" alt="..." class="">
-        	<h4>Ремонт ТОВАРА</h4>
-        </div>	
+        	<h4>Ремонт ноутбука</h4>
+        </div>
 	</div>
-	
+
+	<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+		<div class="text-center">
+        	<img src="http://placehold.it/220x240" alt="..." class="">
+        	<h4>Ремонт электронных книг</h4>
+        </div>
+	</div>
+
 </div>
 </div>
 <?php get_footer(); // подключаем footer.php ?>
