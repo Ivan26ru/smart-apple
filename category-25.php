3215e080eecd3867616fc1b44c39020fd787a09e
Ivan26ru <it-1ivan@ya.ru>
2016-09-22 01:29:30 +0300

    продублирован шиблон вывода категорий с киртинками подкатегорий

---
+++ category-25.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Шаблон РЕМОНТА ТЕЛЕФОНОВ (category-telefony.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+get_header(); // подключаем header.php ?>
+<!-- Менюшка -->
+
+<?php get_template_part( 'service-category' );//подключаем сервис(Ремонт техники) ?>
+<hr>
+
+<?php get_template_part( 'img-category' );//подключаем сервис(Ремонт техники) ?>
+
+
+<?php //dynamic_sidebar( 'sidebar' ); // сайт бар который пока что не нужен?>
+
+<?php get_footer(); // подключаем footer.php ?>
