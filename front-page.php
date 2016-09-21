69d31da96b832c4ba647019a155edc17f4af6af4
Ivan26ru <it-1ivan@ya.ru>
2016-09-21 13:38:37 +0300

    МНОГОУРОВНЕВОЕ МЕНЮ BOOTSTRAP и закомментил сайт-бары

--- front-page.php
+++ front-page.php
@@ -6,11 +6,13 @@
  */
 get_header(); // подключаем header.php ?>
 <!-- Менюшка -->
+
 <?php get_template_part( 'carousel' );//подключаем карусель ?>
 
 <?php get_template_part( 'service' );//подключаем сервис(Ремонт техники) ?>
+
 <?php get_template_part( 'map' );//подключаем карту ?>
 
-<?php dynamic_sidebar( 'sidebar2' ); ?>
+<?php //dynamic_sidebar( 'sidebar' ); // сайт бар который пока что не нужен?>
 
 <?php get_footer(); // подключаем footer.php ?>
