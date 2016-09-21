cb8eb7a3e23b0bb743690264b2a1747ce940514f
Ivan26ru <it-1ivan@ya.ru>
2016-09-22 02:02:34 +0300

    Надпись Где мы находимся

--- map.php
+++ map.php
@@ -9,5 +9,6 @@
  <!-- carusel -->
  <div class="clearfix"></div>
  <div class="col-md-12 hidden-sm hidden-xs">
+ <h1 class="text-center">Где мы находимся</h1>
  <?php echo do_shortcode('[showyamap][placemark address="Светлоград, Московская улица, 7" coordinates="45.341416,42.845794" iconcontent="Smart Mobile"][/showyamap]'); ?>
  </div>
