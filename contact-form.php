77a4fe0feb48c4dda9dccfc11510ae62c7c64807
Ivan26ru <it-1ivan@yandex.ru>
2016-09-22 09:25:53 +0300

    создан шблон для формы обратной связи

---
+++ contact-form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Форма обратной связи (contack-form.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+?>
+<!-- Форма обратной связи -->
+
+<!-- /Форма обратной связи -->
