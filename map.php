10fe44e45e11b532370646950e9e0770e524b332
Ivan26ru <it-1ivan@ya.ru>
2016-09-27 16:08:39 +0300

    переименовал метку на карте на KeliON

--- map.php
+++ map.php
@@ -10,5 +10,5 @@
  <div class="clearfix"></div>
  <div class="col-md-12 hidden-sm hidden-xs">
  <h1 class="text-center">Где мы находимся</h1>
- <?php echo do_shortcode('[showyamap][placemark address="Светлоград, Московская улица, 7" coordinates="45.341416,42.845794" iconcontent="Smart Mobile"][/showyamap]'); ?>
+ <?php echo do_shortcode('[showyamap][placemark address="Светлоград, Московская улица, 7" coordinates="45.341416,42.845794" iconcontent="KeliON"][/showyamap]'); ?>
  </div>
